8bd30ddda875ac913cc509225a9473d78a63f93a
Abduqayyum <rabduqayum@mail.ru>
2022-08-11 13:01:47 +0500

    done

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ryzrfxo#t9n9c0zsm@o==zpgxkez3d#pzn7138_1d^zp7jdehn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["studyportal-abduqayum.herokuapp.com"]
 
 
 # Application definition
